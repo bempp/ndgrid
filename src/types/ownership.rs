42b47237bdc0f6b4a055f7cc33d81b5422ce59cb
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-06-20 16:23:58 +0100

    docstring

--- src/types/ownership.rs
+++ src/types/ownership.rs
@@ -5,6 +5,7 @@
 pub enum Ownership {
     /// Owned by the current process
     Owned,
-    /// Ghost on the current process. The two values are the process that owns this and the local index on that process
+    /// Not owned on the current process. The two values are the process that owns it
+    /// and its local index on that process
     Ghost(usize, usize),
 }
