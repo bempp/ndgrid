74b9d9ba4d63284d8f04df79b0dcae71d40d4f15
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-07-23 11:05:56 +0100

    use macro to generate more tests

--- src/geometry/single_element.rs
+++ src/geometry/single_element.rs
@@ -176,14 +176,86 @@ impl<'g, T: RealScalar, E: FiniteElement> Geometry for SingleElementEntityGeomet
 mod test {
     use super::*;
     use approx::assert_relative_eq;
+    use itertools::izip;
     use ndelement::{
         ciarlet::{CiarletElement, LagrangeElementFamily},
         types::Continuity,
     };
-    use rlst::{RandomAccessMut, DefaultIterator};
-    use itertools::izip;
+    use rlst::{DefaultIterator, RandomAccessMut};
 
-    fn example_geometry_triangle2d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+    fn example_geometry_linear_interval1d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+        let mut points = rlst_dynamic_array2!(f64, [1, 3]);
+        *points.get_mut([0, 0]).unwrap() = 0.0;
+        *points.get_mut([0, 1]).unwrap() = 1.0;
+        *points.get_mut([0, 2]).unwrap() = 2.0;
+        let family = LagrangeElementFamily::<f64>::new(1, Continuity::Standard);
+        SingleElementGeometry::<f64, CiarletElement<f64>>::new(
+            ReferenceCellType::Interval,
+            points,
+            &[0, 1, 1, 2],
+            &family,
+        )
+    }
+
+    fn example_geometry_linear_interval2d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+        let mut points = rlst_dynamic_array2!(f64, [2, 3]);
+        *points.get_mut([0, 0]).unwrap() = 0.0;
+        *points.get_mut([1, 0]).unwrap() = 0.0;
+        *points.get_mut([0, 1]).unwrap() = 1.0;
+        *points.get_mut([1, 1]).unwrap() = 1.0;
+        *points.get_mut([0, 2]).unwrap() = 2.0;
+        *points.get_mut([1, 2]).unwrap() = 0.0;
+        let family = LagrangeElementFamily::<f64>::new(1, Continuity::Standard);
+        SingleElementGeometry::<f64, CiarletElement<f64>>::new(
+            ReferenceCellType::Interval,
+            points,
+            &[0, 1, 1, 2],
+            &family,
+        )
+    }
+
+    fn example_geometry_linear_interval3d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+        let mut points = rlst_dynamic_array2!(f64, [3, 3]);
+        *points.get_mut([0, 0]).unwrap() = 0.0;
+        *points.get_mut([1, 0]).unwrap() = 0.0;
+        *points.get_mut([2, 0]).unwrap() = 1.0;
+        *points.get_mut([0, 1]).unwrap() = 1.0;
+        *points.get_mut([1, 1]).unwrap() = 1.0;
+        *points.get_mut([2, 1]).unwrap() = 0.0;
+        *points.get_mut([0, 2]).unwrap() = 2.0;
+        *points.get_mut([1, 2]).unwrap() = 0.0;
+        *points.get_mut([2, 2]).unwrap() = 0.0;
+        let family = LagrangeElementFamily::<f64>::new(1, Continuity::Standard);
+        SingleElementGeometry::<f64, CiarletElement<f64>>::new(
+            ReferenceCellType::Interval,
+            points,
+            &[0, 1, 1, 2],
+            &family,
+        )
+    }
+
+    fn example_geometry_quadratic_interval2d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+        let mut points = rlst_dynamic_array2!(f64, [2, 5]);
+        *points.get_mut([0, 0]).unwrap() = 0.0;
+        *points.get_mut([1, 0]).unwrap() = 0.0;
+        *points.get_mut([0, 1]).unwrap() = 1.0;
+        *points.get_mut([1, 1]).unwrap() = 0.0;
+        *points.get_mut([0, 2]).unwrap() = 0.5;
+        *points.get_mut([1, 2]).unwrap() = 0.5;
+        *points.get_mut([0, 3]).unwrap() = 2.0;
+        *points.get_mut([1, 3]).unwrap() = 0.0;
+        *points.get_mut([0, 4]).unwrap() = 1.5;
+        *points.get_mut([1, 4]).unwrap() = 0.5;
+        let family = LagrangeElementFamily::<f64>::new(2, Continuity::Standard);
+        SingleElementGeometry::<f64, CiarletElement<f64>>::new(
+            ReferenceCellType::Interval,
+            points,
+            &[0, 1, 2, 1, 3, 4],
+            &family,
+        )
+    }
+
+    fn example_geometry_flat_triangle2d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
         let mut points = rlst_dynamic_array2!(f64, [2, 4]);
         *points.get_mut([0, 0]).unwrap() = 0.0;
         *points.get_mut([1, 0]).unwrap() = 0.0;
@@ -202,7 +274,7 @@ mod test {
         )
     }
 
-    fn example_geometry_triangle3d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+    fn example_geometry_flat_triangle3d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
         let mut points = rlst_dynamic_array2!(f64, [3, 4]);
         *points.get_mut([0, 0]).unwrap() = 0.0;
         *points.get_mut([1, 0]).unwrap() = 0.0;
@@ -225,42 +297,87 @@ mod test {
         )
     }
 
-    #[test]
-    fn test_points_triangle3d() {
-        let g = example_geometry_triangle3d();
+    fn example_geometry_quadratic_triangle2d() -> SingleElementGeometry<f64, CiarletElement<f64>> {
+        let mut points = rlst_dynamic_array2!(f64, [2, 9]);
+        *points.get_mut([0, 0]).unwrap() = 0.0;
+        *points.get_mut([1, 0]).unwrap() = 0.0;
+        *points.get_mut([0, 1]).unwrap() = 1.0;
+        *points.get_mut([1, 1]).unwrap() = 0.0;
+        *points.get_mut([0, 2]).unwrap() = 0.0;
+        *points.get_mut([1, 2]).unwrap() = 1.0;
+        *points.get_mut([0, 3]).unwrap() = 0.5;
+        *points.get_mut([1, 3]).unwrap() = 0.5;
+        *points.get_mut([0, 4]).unwrap() = -0.2;
+        *points.get_mut([1, 4]).unwrap() = 0.5;
+        *points.get_mut([0, 5]).unwrap() = 0.5;
+        *points.get_mut([1, 5]).unwrap() = 0.2;
+        *points.get_mut([0, 6]).unwrap() = 1.0;
+        *points.get_mut([1, 6]).unwrap() = 1.0;
+        *points.get_mut([0, 7]).unwrap() = 1.2;
+        *points.get_mut([1, 7]).unwrap() = 0.5;
+        *points.get_mut([0, 8]).unwrap() = 0.5;
+        *points.get_mut([1, 8]).unwrap() = 1.2;
+        let family = LagrangeElementFamily::<f64>::new(2, Continuity::Standard);
+        SingleElementGeometry::<f64, CiarletElement<f64>>::new(
+            ReferenceCellType::Triangle,
+            points,
+            &[0, 1, 2, 3, 4, 5, 2, 0, 6, 7, 8, 3],
+            &family,
+        )
+    }
 
-        let mut cell_vertices = vec![];
-        for cell in g.cells().col_iter() {
-            let mut pts = vec![];
-            for i in cell.iter() {
-                let mut pt = vec![];
-                for j in 0..g.points().shape()[0] {
-                    pt.push(g.points()[[j, i]]);
-                }
-                pts.push(pt)
-            }
-            cell_vertices.push(pts);
-        }
-        let conn = reference_cell::connectivity(g.element().cell_type());
+    macro_rules! make_tests {
+        ($cellname:ident) => {
+            paste::item! {
+                #[test]
+                fn [< test_points_ $cellname >]() {
+                    //! Test that sub-entities have the correct points
+                    let g = [< example_geometry_ $cellname >]();
 
-        let mut point = vec![0.0; 3];
-        for (cell_i, vertices) in cell_vertices.iter().enumerate() {
+                    let gdim = g.points().shape()[0];
+                    let mut cell_vertices = vec![];
+                    for cell in g.cells().col_iter() {
+                        let mut pts = vec![];
+                        for i in cell.iter() {
+                            let mut pt = vec![];
+                            for j in 0..gdim {
+                                pt.push(g.points()[[j, i]]);
+                            }
+                            pts.push(pt)
+                        }
+                        cell_vertices.push(pts);
+                    }
+                    let conn = reference_cell::connectivity(g.element().cell_type());
 
-            for (dim, conn_dim) in conn.iter().enumerate() {
-                for (index, entity_vertices) in conn_dim.iter().enumerate() {
-                    let entity = SingleElementEntityGeometry::<f64, CiarletElement<f64>>::new(
-                        &g, cell_i, dim, index,
-                    );
-                    for (v, pt) in izip!(&entity_vertices[0], entity.points()) {
-                        pt.coords(&mut point);
-                        for (i, j) in izip!(&point, &vertices[*v]) {
-                           assert_relative_eq!(*i, j);
+                    let mut point = vec![0.0; gdim];
+                    for (cell_i, vertices) in cell_vertices.iter().enumerate() {
+
+                        for (dim, conn_dim) in conn.iter().enumerate() {
+                            for (index, entity_vertices) in conn_dim.iter().enumerate() {
+                                let entity = SingleElementEntityGeometry::<f64, CiarletElement<f64>>::new(
+                                    &g, cell_i, dim, index,
+                                );
+                                for (v, pt) in izip!(&entity_vertices[0], entity.points()) {
+                                    pt.coords(&mut point);
+                                    for (i, j) in izip!(&point, &vertices[*v]) {
+                                       assert_relative_eq!(*i, j);
+                                    }
+                                }
+
+
+                            }
                         }
                     }
-
-
                 }
             }
-        }
+        };
     }
+
+    make_tests!(linear_interval1d);
+    make_tests!(linear_interval2d);
+    make_tests!(linear_interval3d);
+    make_tests!(quadratic_interval2d);
+    make_tests!(flat_triangle2d);
+    make_tests!(flat_triangle3d);
+    make_tests!(quadratic_triangle2d);
 }
