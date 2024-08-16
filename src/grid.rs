9a16fc2008eda72a5f6615483a2de770d68a87c3
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-08-16 10:39:04 +0100

    fmt

--- src/grid.rs
+++ src/grid.rs
@@ -2,5 +2,5 @@
 #[cfg(feature = "mpi")]
 pub mod parallel;
 pub mod serial;
+pub use parallel::ParallelGrid;
 pub use serial::{SingleElementGrid, SingleElementGridBuilder};
-pub use parallel::{ParallelGrid};
