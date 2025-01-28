563725338b52e5593df7a6a8088242a1e171badb
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-01-28 16:39:29 +0000

    debug

--- examples/parallel_grid.rs
+++ examples/parallel_grid.rs
@@ -16,31 +16,37 @@ fn main() {
 
     let mut b = SingleElementGridBuilder::<f64>::new(2, (ReferenceCellType::Quadrilateral, 1));
 
-    let mut i = 0;
-    for y in 0..n {
-        for x in 0..n {
-            b.add_point(i, &[x as f64 / (n - 1) as f64, y as f64 / (n - 1) as f64]);
-            i += 1;
-        }
-    }
-
-    let mut i = 0;
-    for y in 0..n - 1 {
-        for x in 0..n - 1 {
-            let sw = n * y + x;
-            b.add_cell(i, &[sw, sw + 1, sw + n, sw + n + 1]);
-            i += 1;
-        }
-    }
-
     let universe: Universe = mpi::initialize().unwrap();
     let comm = universe.world();
     let rank = comm.rank();
+    println!("{rank} A");
     let grid = if rank == 0 {
+        let mut i = 0;
+        for y in 0..n {
+            for x in 0..n {
+                b.add_point(i, &[x as f64 / (n - 1) as f64, y as f64 / (n - 1) as f64]);
+                i += 1;
+            }
+        }
+
+        let mut i = 0;
+        for y in 0..n - 1 {
+            for x in 0..n - 1 {
+                let sw = n * y + x;
+                b.add_cell(i, &[sw, sw + 1, sw + n, sw + n + 1]);
+                i += 1;
+            }
+        }
+
+        println!("{rank} B");
         b.create_parallel_grid_root(&comm, GraphPartitioner::None)
+        println!("{rank} C");
     } else {
+        println!("{rank} B");
         b.create_parallel_grid(&comm, 0)
+        println!("{rank} C");
     };
+    println!("{rank} D");
 
     // Check that owned cells are sorted ahead of ghost cells
 
