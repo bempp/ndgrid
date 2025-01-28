8f1efc168525cded3478db7bd413581986d4b7b1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-01-28 17:13:23 +0000

    not coupe

--- examples/parallel_grid.rs
+++ examples/parallel_grid.rs
@@ -1,4 +1,4 @@
-//? mpirun -n {{NPROCESSES}} --features "serde,coupe"
+//? mpirun -n {{NPROCESSES}} --features "serde"
 
 use mpi::{
     collective::SystemOperation, environment::Universe, topology::Communicator,
@@ -39,7 +39,7 @@ fn main() {
         }
 
         println!("{rank} B");
-        b.create_parallel_grid_root(&comm, GraphPartitioner::Coupe)
+        b.create_parallel_grid_root(&comm, GraphPartitioner::None)
     } else {
         println!("{rank} B");
         b.create_parallel_grid(&comm, 0)
