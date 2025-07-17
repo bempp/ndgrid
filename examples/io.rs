1703ab14765b35c3618104ebb1c5ebf59aa5eb99
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-07-17 16:21:42 +0100

    add I/O example

---
+++ examples/io.rs
@@ -0,0 +1,50 @@
+use itertools::izip;
+use ndelement::{types::ReferenceCellType, ciarlet::CiarletElement, map::IdentityMap};
+use ndgrid::traits::{Entity, Grid, Topology, RONExport, RONImport, GmshExport, GmshImport, Builder};
+use ndgrid::{shapes, SingleElementGrid, SingleElementGridBuilder};
+
+/// Grid I/O
+///
+/// Demonstration of importing and exporting a grid in serial
+///
+/// Parallel I/O is demonstrated in the example `parallel_io.rs`
+fn main() {
+    // Create a grid using the shapes module: unit_cube_boundary will mesh the surface of a cube
+    let g = shapes::unit_cube_boundary::<f64>(4, 5, 4, ReferenceCellType::Triangle);
+
+    // If the serde option is used, the raw grid data can be exported in RON format
+    g.export_as_ron("_unit_cube_boundary.ron");
+
+    // A grid can be re-imported from raw RON data
+    let g2 = SingleElementGrid::<f64, CiarletElement<f64, IdentityMap>>::import_from_ron("_unit_cube_boundary.ron");
+
+    // Print the first 5 cells of each grid
+    println!("The first 5 cells of the grids");
+    for (cell, cell2) in izip!(g.cell_iter(), g2.cell_iter()).take(5) {
+        println!("{:?} {:?}",
+            cell.topology().sub_entity_iter(0).collect::<Vec<_>>(),
+            cell2.topology().sub_entity_iter(0).collect::<Vec<_>>(),
+        );
+    }
+
+    println!();
+
+    // Alternatively, grids can be exported and imported to/from Gmsh files
+
+    // Export the grid as a Gmsh .msh file
+    g.export_as_gmsh("_unit_cube_boundary.msh");
+
+    // To import from a Gmsh .msh file, a builder is used
+    let mut b = SingleElementGridBuilder::<f64>::new(3, (ReferenceCellType::Triangle, 1));
+    b.import_from_gmsh("_unit_cube_boundary.msh");
+    let g3 = b.create_grid();
+
+    // Print the first 5 cells of each grid
+    println!("The first 5 cells of the grids");
+    for (cell, cell3) in izip!(g.cell_iter(), g3.cell_iter()).take(5) {
+        println!("{:?} {:?}",
+            cell.topology().sub_entity_iter(0).collect::<Vec<_>>(),
+            cell3.topology().sub_entity_iter(0).collect::<Vec<_>>(),
+        );
+    }
+}
